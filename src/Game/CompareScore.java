3eab292b1cb226952edcc64137db9b325c7f0cdd
IVXX_LeGioN <IVXX_LeGioN@192.168.0.20>
2017-06-20 17:54:26 -0400

    Final Version, all cleaned and Ready for submission

--- src/Game/CompareScore.java
+++ src/Game/CompareScore.java
@@ -15,48 +15,48 @@ import sorts.Sorts;
  * @author canto5684
  */
 public class CompareScore extends JPanel { //GUI Class
-    
+
     private int[] scores;
     private String[] scoreStr = new String[5];
     private JButton b1;
     private JLabel title, score1, score2, score3, score4, score5;
-    private static final Color LIME = new Color(153, 255, 51);private static final Color STONE = new Color(128, 128, 128);private GridIO io = new GridIO();
-    
-    
-    
+    private static final Color LIME = new Color(153, 255, 51);
+    private static final Color STONE = new Color(128, 128, 128);
+    private GridIO io = new GridIO();
+
     public CompareScore() {
         setLayout(new GridLayout(0, 1, 10, 10));
-        
+
         title = new JLabel("Scoreboard");
-        title.setFont(new Font("Century Gothic", Font.PLAIN,12));
+        title.setFont(new Font("Century Gothic", Font.PLAIN, 12));
         title.setForeground(LIME);
         add(title);
-        
+
         score1 = new JLabel("1. ----------");
-        score1.setFont(new Font("Century Gothic", Font.PLAIN,12));
+        score1.setFont(new Font("Century Gothic", Font.PLAIN, 12));
         score1.setForeground(LIME);
         add(score1);
-        
+
         score2 = new JLabel("2. ----------");
-        score2.setFont(new Font("Century Gothic", Font.PLAIN,12));
+        score2.setFont(new Font("Century Gothic", Font.PLAIN, 12));
         score2.setForeground(LIME);
         add(score2);
-        
+
         score3 = new JLabel("3. ----------");
-        score3.setFont(new Font("Century Gothic", Font.PLAIN,12));
+        score3.setFont(new Font("Century Gothic", Font.PLAIN, 12));
         score3.setForeground(LIME);
         add(score3);
-        
+
         score4 = new JLabel("4. ----------");
-        score4.setFont(new Font("Century Gothic", Font.PLAIN,12));
+        score4.setFont(new Font("Century Gothic", Font.PLAIN, 12));
         score4.setForeground(LIME);
         add(score4);
-        
+
         score5 = new JLabel("5. ----------");
-        score5.setFont(new Font("Century Gothic", Font.PLAIN,12));
+        score5.setFont(new Font("Century Gothic", Font.PLAIN, 12));
         score5.setForeground(LIME);
         add(score5);
-        
+
         b1 = new JButton("Refresh Scores");
         b1.setFont(new Font("Century Gothic", Font.PLAIN, 12));
         b1.setBackground(STONE);
@@ -67,8 +67,6 @@ public class CompareScore extends JPanel { //GUI Class
         b1.setActionCommand("refresh");
         add(b1);
     }
-    
-
 
     class BtnListener implements ActionListener {
 
@@ -80,7 +78,7 @@ public class CompareScore extends JPanel { //GUI Class
                 case "refresh":
                     scores = io.readScore(); //Reads the scores from the file
                     Sorts.insertionSort(scores);//Sorts the scores low to high
-                    score1.setText("1. "+ scores[4]);
+                    score1.setText("1. " + scores[4]);
                     score2.setText("2. " + scores[3]);
                     score3.setText("3. " + scores[2]);
                     score4.setText("4. " + scores[1]);
